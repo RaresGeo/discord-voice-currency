8b498ab16fab1a8e124ff16f358b611a223dc63b
Rares <raresgeo9@gmail.com>
2022-05-13 22:37:34 +0300

    fix: Shop inconsistencies

--- commands/buy.js
+++ commands/buy.js
@@ -1,45 +1,73 @@
-"use strict";
+'use strict';
 
-const config = require("../utilities/config").config;
-const User = require("../models/User");
-const utils = require("../utilities/utils");
-const { incorrectSyntax, finished } = require("../utilities/emojis");
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
-const jsonfile = require("jsonfile");
+const config = require('../utilities/config').config;
+const Role = require('../models/Role');
+const utils = require('../utilities/utils');
+const { incorrectSyntax, finished } = require('../utilities/emojis');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const jsonfile = require('jsonfile');
 
 module.exports = {
-  aliases: ["buy"],
-  event: "messageCreate",
+  aliases: ['buy'],
+  event: 'messageCreate',
 };
 
+const ROLES_JSON_FILE = './db/json/roles.json';
+
 module.exports.command = async (message) => {
-  let yesButton = new MessageButton().setCustomId("yes").setEmoji(finished).setStyle("PRIMARY");
-  let noButton = new MessageButton().setCustomId("no").setEmoji(incorrectSyntax).setStyle("PRIMARY");
+  let yesButton = new MessageButton()
+    .setCustomId('yes')
+    .setEmoji(finished)
+    .setStyle('PRIMARY');
+  let noButton = new MessageButton()
+    .setCustomId('no')
+    .setEmoji(incorrectSyntax)
+    .setStyle('PRIMARY');
 
   let guildId = message.guildId;
-  let roleName = utils.parseArgs(message.content)._.join(" ");
+  let roleName = utils.parseArgs(message.content)._.join(' ');
 
-  let roles = jsonfile.readFileSync("./db/json/roles.json");
-  let role = roles.find(({ guild_id, name }) => guild_id === guildId && name.toLowerCase() === roleName.toLowerCase());
-  if (!role) {
-    utils.sendDelete(message, "Could not find role. Are you sure it is in the shop?");
-    return;
-  }
-  let _role = await message.guild.roles.fetch(role.role_id).catch((err) => console.log(err));
-  if (!_role) {
-    utils.sendDelete(message, "Server doesn't have the role. Was it removed?");
-    return;
-  }
+  let roles = jsonfile.readFileSync(ROLES_JSON_FILE);
+  let _role;
+  let role;
+  do {
+    let roleIndex = roles.findIndex(
+      ({ guild_id, name }) =>
+        guild_id === guildId && name.toLowerCase() === roleName.toLowerCase()
+    );
+    if (roleIndex === -1) {
+      utils.sendDelete(
+        message,
+        'Could not find role. Are you sure it is in the shop?'
+      );
+      return;
+    }
+    role = roles[roleIndex];
+    _role = await message.guild.roles
+      .fetch(role.role_id)
+      .catch((err) => console.log(err));
+    if (!_role) {
+      // Role wasn't found but it's possible it's still in the shop. Let's find and remove it. Check if there's a new
+      Role.deleteOne({ guild_id: guildId, role_id: role.role_id });
+      roles.splice(roleIndex, 1);
+      jsonfile.writeFileSync(ROLES_JSON_FILE, roles, { spaces: 2 });
+    } else {
+      break;
+    }
+  } while (true);
 
   let embed = new MessageEmbed()
     .setColor(_role.hexColor)
     .setTitle(`Buy ${_role.name}?`)
-    .addField("Price", `💵  ${role.price}`)
-    .addField("Stock", `${role.stock} roles`)
-    .addField("Multiplier", `${role.multiplier}x`);
+    .addField('Price', `💵  ${role.price}`)
+    .addField('Stock', `${role.stock} roles`)
+    .addField('Multiplier', `${role.multiplier}x`);
 
   let row = new MessageActionRow().addComponents(yesButton, noButton);
-  let initialMessage = await message.channel.send({ embeds: [embed], components: [row] });
+  let initialMessage = await message.channel.send({
+    embeds: [embed],
+    components: [row],
+  });
 
   const filter = (interaction) => interaction.user === message.author;
 
@@ -47,24 +75,26 @@ module.exports.command = async (message) => {
     filter,
   });
 
-  buttonCollector.on("collect", async (interaction) => {
+  buttonCollector.on('collect', async (interaction) => {
     let selectedButton;
 
     switch (interaction.customId) {
-      case "yes":
+      case 'yes':
         selectedButton = yesButton;
         buyRole(message, _role, role.price);
         break;
-      case "no":
+      case 'no':
         selectedButton = noButton;
         break;
     }
 
     let row = new MessageActionRow().addComponents(selectedButton);
 
-    await interaction.update({ components: [row] }).catch((err) => console.log(err));
+    await interaction
+      .update({ components: [row] })
+      .catch((err) => console.log(err));
 
-    buttonCollector.stop(["Collected reaction"]);
+    buttonCollector.stop(['Collected reaction']);
   });
 };
 
@@ -89,7 +119,7 @@ const buyRole = async (message, role, price) => {
 
   if (success) {
     utils.sendDelete(message, `Successfully bought ${role.name}.`);
-    roleManager.add(role, "Bought with points");
+    roleManager.add(role, 'Bought with points');
   } else {
     utils.sendDelete(message, `Not enough points.`);
   }
--- commands/shop.js
+++ commands/shop.js
@@ -1,24 +1,32 @@
-"use strict";
+'use strict';
 
-const config = require("../utilities/config").config;
-const User = require("../models/User");
-const utils = require("../utilities/utils");
-const { leftArrow, rightArrow } = require("../utilities/emojis");
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
-const jsonfile = require("jsonfile");
+const config = require('../utilities/config').config;
+const User = require('../models/User');
+const utils = require('../utilities/utils');
+const { leftArrow, rightArrow } = require('../utilities/emojis');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const jsonfile = require('jsonfile');
 
 const ROLES_PER_PAGE = 6;
-const numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"];
+const numbers = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
 
 module.exports = {
-  aliases: ["shop"],
-  event: "messageCreate",
+  aliases: ['shop'],
+  event: 'messageCreate',
 };
 
 module.exports.command = async (message) => {
-  let leftButton = new MessageButton().setCustomId("left").setEmoji(leftArrow).setStyle("PRIMARY");
-  let rightButton = new MessageButton().setCustomId("right").setEmoji(rightArrow).setStyle("PRIMARY");
-  let _roles = jsonfile.readFileSync("./db/json/roles.json");
+  let leftButton = new MessageButton()
+    .setCustomId('left')
+    .setEmoji(leftArrow)
+    .setStyle('PRIMARY');
+  let rightButton = new MessageButton()
+    .setCustomId('right')
+    .setEmoji(rightArrow)
+    .setStyle('PRIMARY');
+  let _roles = jsonfile
+    .readFileSync('./db/json/roles.json')
+    .filter(({ guild_id }) => guild_id === message.guildId);
   // Construct embed
 
   let indexStart = 0;
@@ -26,8 +34,18 @@ module.exports.command = async (message) => {
   let roles = _roles.slice(indexStart, indexStart + ROLES_PER_PAGE);
   let embed = generateEmbed(message, roles, indexStart, _roles.length);
 
-  let row = new MessageActionRow().addComponents(leftButton, rightButton, new MessageButton().setCustomId("number").setEmoji(numbers[0]).setStyle("SECONDARY"));
-  let initialMessage = await message.channel.send({ embeds: [embed], components: [row] });
+  let row = new MessageActionRow().addComponents(
+    leftButton,
+    rightButton,
+    new MessageButton()
+      .setCustomId('number')
+      .setEmoji(numbers[0])
+      .setStyle('SECONDARY')
+  );
+  let initialMessage = await message.channel.send({
+    embeds: [embed],
+    components: [row],
+  });
 
   const filter = (interaction) => interaction.user === message.author;
 
@@ -35,35 +53,47 @@ module.exports.command = async (message) => {
     filter,
   });
 
-  buttonCollector.on("collect", async (interaction) => {
+  buttonCollector.on('collect', async (interaction) => {
     switch (interaction.customId) {
-      case "left":
+      case 'left':
         indexStart -= ROLES_PER_PAGE;
         if (indexStart < 0) indexStart = 0;
         break;
-      case "right":
+      case 'right':
         indexStart += ROLES_PER_PAGE;
-        if (indexStart + ROLES_PER_PAGE >= _roles.length) indexStart = _roles.length - ROLES_PER_PAGE;
+        if (indexStart + ROLES_PER_PAGE >= _roles.length)
+          indexStart = _roles.length - ROLES_PER_PAGE;
         break;
     }
 
     roles = _roles.slice(indexStart, indexStart + ROLES_PER_PAGE);
     embed = generateEmbed(message, roles, indexStart, _roles.length);
-    await initialMessage.edit({ embeds: [embed] }).catch((err) => console.log(err));
+    await initialMessage
+      .edit({ embeds: [embed] })
+      .catch((err) => console.log(err));
     row = new MessageActionRow().addComponents(
       leftButton,
       rightButton,
       new MessageButton()
-        .setCustomId("number")
+        .setCustomId('number')
         .setEmoji(numbers[Math.ceil(indexStart / ROLES_PER_PAGE)])
-        .setStyle("SECONDARY")
+        .setStyle('SECONDARY')
     );
-    await interaction.update({ components: [row] }).catch((err) => console.log(err));
+    await interaction
+      .update({ components: [row] })
+      .catch((err) => console.log(err));
   });
 };
 
 const generateEmbed = (message, roles, indexStart, rolesLength) => {
-  let embed = new MessageEmbed().setColor("DARK_BUT_NOT_BLACK").setTitle(`${message.guild.name} shop`).setFooter(`Showing ${indexStart + 1} to ${indexStart + ROLES_PER_PAGE} out of ${rolesLength} roles`);
+  let embed = new MessageEmbed()
+    .setColor('DARK_BUT_NOT_BLACK')
+    .setTitle(`${message.guild.name} shop`)
+    .setFooter(
+      `Showing ${indexStart + 1} to ${
+        indexStart + ROLES_PER_PAGE
+      } out of ${rolesLength} roles`
+    );
 
   roles.forEach(({ guild_id, role_id, name, price, stock, multiplier }) => {
     if (guild_id === message.guildId) {
